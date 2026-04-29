554557c65d023b69a895620ffda9fee5b7ba4664
Swapnil <109898004+Swapnilpy31@users.noreply.github.com>
2026-04-29 15:33:12 +0530

    mobile view

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     
     window.addEventListener('scroll', () => {
         if (window.scrollY > 10) {
-            header.style.boxShadow = '0 4px 6px -1px rgba(0,0,0,0.1)';
+            header.style.boxShadow = '0 4px 12px rgba(0,0,0,0.35)';
         } else {
-            header.style.boxShadow = '0 1px 3px rgba(0,0,0,0.1)';
+            header.style.boxShadow = '0 2px 12px rgba(0,0,0,0.25)';
         }
     });
 
